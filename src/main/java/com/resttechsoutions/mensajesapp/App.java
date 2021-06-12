1a8fb7dde20f0e4740150b8ef5787ff8d97382f1
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-06-12 18:24:25 -0400

    JavaPersistencePlatzi

---
+++ src/main/java/com/resttechsoutions/mensajesapp/App.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.resttechsoutions.mensajesapp;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Rafael Estrella
+ */
+public class App {
+    public static void main(String[] args) {
+        MySqlConnection con = new MySqlConnection();
+        
+        try (Connection cnx = con.getConnection()) {
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
